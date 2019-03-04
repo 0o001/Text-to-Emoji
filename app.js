442eef35de0d8e5c5547f5c0b59ef06105375b52
Mustafa Uzun <mustafauzun0@gmail.com>
2019-03-04 17:25:46 +0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,39 @@
+const readline = require('readline')
+const emoji = require('./emoji.json')
+
+const rl = readline.createInterface({
+
+    input: process.stdin,
+    output: process.stdout,
+
+})
+
+rl.question('> ', (answer) => {
+
+    let textToEmoji = ''
+    const answerArray = answer.split(' ').filter( item => item )
+    
+    answerArray.forEach( ( answerItem ) => {
+
+        const findEmojiArray = Object.values(emoji).filter( ( emojiItem ) => {
+
+            return answerItem.includes( ...emojiItem.keywords )
+
+        })
+
+        if( findEmojiArray.length ) {
+
+            const char = findEmojiArray[Math.floor( Math.random() * findEmojiArray.length )].char
+
+            textToEmoji += char + ' '
+
+        }
+
+    })
+
+    console.log( textToEmoji )
+     
+    rl.close()
+    
+})
+
